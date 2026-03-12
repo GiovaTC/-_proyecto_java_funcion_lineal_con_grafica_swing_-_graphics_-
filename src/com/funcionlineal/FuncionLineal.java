7c802fd035f805174dbe5a8bdf0a685a062c517d
gtapiero <giovanny.tapiero@pi.edu.co>
2026-03-12 09:39:03 -0500

    commit - FuncionLineal .

--- src/com/funcionlineal/FuncionLineal.java
+++ src/com/funcionlineal/FuncionLineal.java
@@ -1,4 +1,16 @@
 package com.funcionlineal;
 
 public class FuncionLineal {
-}
+
+    private double a;
+    private double b;
+
+    public FuncionLineal(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public double calcular(double x) {
+        return a * x + b;
+    }
+}   
